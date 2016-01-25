0b0301888c447056c852ad0a235d3f12e29aa0db
Peter Lesslie <pclesslie@gmail.com>
2016-01-24 19:30:34 -0600

    Support captures for non-king pieces

--- main.c
+++ main.c
@@ -28,6 +28,7 @@ struct move_t {
     char y1;
     char x2;
     char y2;
+    uint8_t capture; // 255 if no capture, otherwise location of piece captured
 };
 
 board_t MOVES[] = { 0,1280,0,5120,0,20480,0,16384,131074,0,655370,0,2621480,0,10485920,0,0,83887360,0,335549440,0,1342197760,0,1073758208,8590065664,0,42950328320,0,171801313280,0,687205253120,0,0,5497642024960,0,21990568099840,0,87962272399360,0,70369817919488,562958543355904,0,2814792716779520,0,11259170867118080,0,45036683468472320,0,0,360293467747778560,0,1441173870991114240,0,5764695483964456960,0,4611756387171565568,562949953421312,0,2814749767106560,0,11258999068426240,0,45035996273704960,0 };
@@ -36,12 +37,13 @@ board_t WHITE_MOVES[] = { 0,0,0,0,0,0,0,0,2,0,10,0,40,0,160,0,0,1280,0,5120,0,20
 
 board_t BLACK_MOVES[] = { 0,1280,0,5120,0,20480,0,16384,131072,0,655360,0,2621440,0,10485760,0,0,83886080,0,335544320,0,1342177280,0,1073741824,8589934592,0,42949672960,0,171798691840,0,687194767360,0,0,5497558138880,0,21990232555520,0,87960930222080,0,70368744177664,562949953421312,0,2814749767106560,0,11258999068426240,0,45035996273704960,0,0,360287970189639680,0,1441151880758558720,0,5764607523034234880,0,4611686018427387904,0,0,0,0,0,0,0,0 };
 
+#define CAPTURE(move) ((move).capture != 255)
 #define MASK(square) ((board_t)1 << (square))
 #define CAN_MOVE(to, from) (MOVES[(from)] & MASK(to))
 #define WHITE_CAN_MOVE(to, from) (WHITE_MOVES[(from)] & MASK(to))
-#define BLACK_CAN_MOVE(to, from) (BLACK_MOVES[(from)] & MASK(from))
-#define BLACK_MOVE(move) ((move) & 1)
-#define WHITE_MOVE(move) (!BLACK_MOVE(move))
+#define BLACK_CAN_MOVE(to, from) (BLACK_MOVES[(from)] & MASK(to))
+#define BLACK_MOVE(state) ((state).move & 1)
+#define WHITE_MOVE(state) (!BLACK_MOVE(state))
 #define IS_SET(board, square) ((board) & MASK(square))
 #define SET(board, square) (board |= MASK(square))
 #define CLEAR(board, square) (board &= ~MASK(square))
@@ -56,6 +58,12 @@ board_t BLACK_MOVES[] = { 0,1280,0,5120,0,20480,0,16384,131072,0,655360,0,262144
     (state).black = 11163050UL;                 \
     (state).black_kings = 0UL;                  \
     (state).move  = 0
+#define move_init(move)                         \
+    (move).x1 = 0;                              \
+    (move).y1 = 0;                              \
+    (move).x2 = 0;                              \
+    (move).y2 = 0;                              \
+    (move).capture = 255;
 #define VALID_MOVES 6172840429334713770UL
 #define VALID(square) (((square_t)(square) & VALID_MOVES))
 #define SQUARE(x, y) ((y)*8 + (x))
@@ -65,10 +73,10 @@ board_t BLACK_MOVES[] = { 0,1280,0,5120,0,20480,0,16384,131072,0,655360,0,262144
 #define BLACK(board) ((board_t)((board).black | (board).black_kings))
 #define FULLBOARD(board) (WHITE(board) | BLACK(board))
 #define OCCUPIED(square, board) (MASK(square) & FULLBOARD(board))
-#define UP_LEFT(square) ((square) + 9)
-#define UP_RIGHT(square) ((square) + 7)
-#define DOWN_LEFT(square) ((square) - 7)
-#define DOWN_RIGHT(square) ((square) - 9)
+#define DOWN_LEFT(square) ((square) + 9)
+#define DOWN_RIGHT(square) ((square) + 7)
+#define UP_LEFT(square) ((square) - 7)
+#define UP_RIGHT(square) ((square) - 9)
 
 void print_board(struct state_t* state) {
     int i;
@@ -106,14 +114,19 @@ int legal_move(struct state_t* state, struct move_t* move) {
     if (!VALID(to) || OCCUPIED(to, *state)) {
         ret = 0;
     }
-    else if (WHITE_MOVE(state->move)) {
+    else if (WHITE_MOVE(*state)) {
         printf("Checking white move\n");
         /* TODO: check jumps, double jumps, kings */
+        printf("to = %lu, from = %lu, mask = %lu\n", to, from, WHITE_MOVES[from]);
+        printf("up_left(up_left(from)) = %lu\n", UP_LEFT(UP_LEFT(from)));
+        printf("up_right(up_right(from)) = %lu\n", UP_RIGHT(UP_RIGHT(from)));
         if (WHITE_CAN_MOVE(to, from)) {
             ret = 1;
         } else if (to == UP_LEFT(UP_LEFT(from)) && IS_SET(BLACK(*state), UP_LEFT(from))) {
+            move->capture = UP_LEFT(from);
             ret = 1;
         } else if (to == UP_RIGHT(UP_RIGHT(from)) && IS_SET(BLACK(*state), UP_RIGHT(from))) {
+            move->capture = UP_RIGHT(from);
             ret = 1;
         } else {
             /* check king moves */
@@ -123,7 +136,14 @@ int legal_move(struct state_t* state, struct move_t* move) {
     else { /* black move */
         printf("Checking black move\n");
         /* TODO: check jumps, double jumps, kings */
+        printf("to = %lu, from = %lu, mask = %lu\n", to, from, BLACK_MOVES[from]);
         if (BLACK_CAN_MOVE(to, from)) {
+            ret = 1;
+        } else if (to == DOWN_LEFT(DOWN_LEFT(from)) && IS_SET(WHITE(*state), DOWN_LEFT(from))) {
+            move->capture = DOWN_LEFT(from);
+            ret = 1;
+        } else if (to == DOWN_RIGHT(DOWN_RIGHT(from)) && IS_SET(WHITE(*state), DOWN_RIGHT(from))) {
+            move->capture = DOWN_RIGHT(from);
             ret = 1;
         } else {
             ret = 0;
@@ -196,12 +216,18 @@ void get_move(struct state_t* state, struct move_t* move) {
 }
 
 void make_move(struct state_t* state, struct move_t* move) {
-    if (WHITE_MOVE(state->move)) {
+    if (WHITE_MOVE(*state)) {
         CLEAR(state->white, SQUARE(move->x1, move->y1));
         SET(state->white, SQUARE(move->x2, move->y2));
+        if (CAPTURE(*move)) {
+            CLEAR(state->black, move->capture);
+        }
     } else {
         CLEAR(state->black, SQUARE(move->x1, move->y1));
         SET(state->black, SQUARE(move->x2, move->y2));
+        if (CAPTURE(*move)) {
+            CLEAR(state->white, move->capture);
+        }
     }
     state->move += 1;
 }
@@ -215,6 +241,14 @@ int main(int argc, char **argv) {
     print_board(&state);
 
     for (i = 0; i < 10; ++i) {
+        printf("Move #%d\n", state.move);
+        if (WHITE_MOVE(state)) {
+            printf("white to move\n");
+        }
+        if (BLACK_MOVE(state)) {
+            printf("black to move\n");
+        }
+        move_init(move);
         get_move(&state, &move);
         make_move(&state, &move);
         print_board(&state);
